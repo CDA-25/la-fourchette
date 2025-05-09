ca752f8d85acf4ed764b8b473b172e7ee54b7d01
Taaazzz <taaazzz.bg@gmail.com>
2025-05-09 15:44:43 +0200

    Rajout de tous mes commentaires pour une meilleur compréhension

--- js/main.js
+++ js/main.js
@@ -17,13 +17,14 @@ function getRandomNumber(min, max) {
 //Etape 3 cree une fonction pour jouer
 function play() {
     // On initialise le jeu
+    //Le nombre a chercher qui vaut de base zero on lui donne la valeur du nombre aleatoire
     game.searchedNumber = getRandomNumber(game.min, game.max);
     game.attempts = 1;
 
-    // on va demander aux joueurs de nous donner un nombre
+    // on va demander aux joueurs de nous donner un nombre en utilisant parseInt pour pouvoir mettre un nombre
     let entredNumber = parseInt(prompt(`Quel est le nombre à trouver entre ${game.min} et ${game.max} ?`));
 
-    // Ici on va cree la boucle du jeu
+    // Ici on va cree la boucle du jeu si le joueur ne trouve pas la bonne valeur dés le début
     while (entredNumber !== game.searchedNumber) {
         // On va lui dire que si plus petit ou plus grand ce qu'il doit faire
         if (entredNumber < game.searchedNumber) {
@@ -31,12 +32,13 @@ function play() {
         } else if (entredNumber > game.searchedNumber) {
             entredNumber = parseInt(prompt('C\'est moins'));
         }
-        // Pour savoir le nombre d'essai du joueur on va incrémenter sa valeur
+        // Pour savoir le nombre d'essai du joueur on va incrémenter sa valeur de 1
         game.attempts += 1;
     }
 
-    // Si le joueur a trouvé le bon nombre
+    // Si le joueur a trouvé le bon nombre dés le premier essai ou apres la boucle
     alert(`Bravo ! C'était bien ${game.searchedNumber} - Nombre d'essais : ${game.attempts}`);
+    //On rentre le score du joueur dans le tableau des scores
     game.scores.push(game.attempts);
 
     // Bonus : Afficher les scores des parties précédentes
@@ -47,12 +49,16 @@ function play() {
     });
     alert(scoreMessage);
 
-    // Si le joueur gagne, on peut lui proposer de rejouer
+    // Si le joueur gagne, on peut lui proposer de rejouer avec la fonction confirm
     if (confirm('Voulez-vous rejouer ?')) {
         play();
+    }else {
+        //Ajout d'une formule de politesse, non mais faut pas abuser non plus
+        alert('Merci d\'avoir joué !');
     }
 }
 
 // maintenant on va enfin lancer notre fonction pour jouer
 play();
-//Remerciement a google pour m'avoir aider a trouver la solution XD
+
+//Remerciement a google pour mes recherches et copilot pour l'amélioration de l'affichage des scores
