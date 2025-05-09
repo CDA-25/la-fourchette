7fe59ec0a1ff3fc989ab992bc1ca18d70cd036c8
Alice <alice.cordier39210@gmail.com>
2025-05-09 14:18:00 +0200

    function nombre aléatoire

--- js/main.js
+++ js/main.js
@@ -1,18 +1,28 @@
 // Le nombre max
-let max = 500;
+
+
 
 // je déclare mon objet Game :
 let game = {
     // Le nombre cherché
-    searchedNumber: Math.round(Math.random() * max),
+    searchedNumber: "",
     // Le nombre d'essais
-    attemps: 1
+    attemps: 1,
+    maxi: 50,
+    mini: 10
 }
 
+function nbrAleatoire(min, max) {
+    return Math.round(Math.random() * (max - min) + min)
+}
+
+game.searchedNumber = nbrAleatoire(game.mini, game.maxi)
 // Le nombre saisi
 let enteredNumber = parseInt(prompt('Quel est le nombre à trouver ?'));
 
+
 function play(jeu) { // on déclare une function pour jouer : on veut utiliser les fonctionnalités par rapport à l'objet
+    nbrAleatoire()
     // Tant que le nombre saisi n'est pas bon on redemande un nombre
     while (enteredNumber !== jeu.searchedNumber) {
         // on précise si le nombre recherché est inférieur ou supérieur au nombre saisi
@@ -31,8 +41,5 @@ function play(jeu) { // on déclare une function pour jouer : on veut utiliser l
 }
 // on est sorti de la boucle, c'est que le nombre saisi est bien le nombre cherché
 // on affiche un message de victoire
-
-
-
 
 play(game)
