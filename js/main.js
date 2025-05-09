cc12c3fbb3ef17d03622b226b2f67638b8ee68f1
Alice <alice.cordier39210@gmail.com>
2025-05-09 13:19:54 +0200

    création : fonction play pour lancer le jeu avec les instructions

--- js/main.js
+++ js/main.js
@@ -12,21 +12,27 @@ let game = {
 // Le nombre saisi
 let enteredNumber = parseInt(prompt('Quel est le nombre à trouver ?'));
 
-
-// Tant que le nombre saisi n'est pas bon on redemande un nombre
-while (enteredNumber !== searchedNumber) {
-    // on précise si le nombre recherché est inférieur ou supérieur au nombre saisi
-    if (enteredNumber < searchedNumber) {
-        enteredNumber = parseInt(prompt('C\'est plus'));
+function play(jeu) { // on déclare une function pour jouer : on veut utiliser les fonctionnalités par rapport à l'objet
+    // Tant que le nombre saisi n'est pas bon on redemande un nombre
+    while (enteredNumber !== jeu.searchedNumber) {
+        // on précise si le nombre recherché est inférieur ou supérieur au nombre saisi
+        if (enteredNumber < jeu.searchedNumber) {
+            enteredNumber = parseInt(prompt('C\'est plus'));
+        }
+        else {
+            enteredNumber = parseInt(prompt('C\'est moins'));
+        }
+        // on incrémente le nombre d'essais
+        jeu.attemps += 1;
     }
-    else {
-        enteredNumber = parseInt(prompt('C\'est moins'));
+    if (enteredNumber === jeu.searchedNumber) {
+        alert('Bravo ! C\'était bien ' + jeu.searchedNumber + ' - Nombre d\'essais : ' + jeu.attemps);
     }
-    // on incrémente le nombre d'essais
-    attemps += 1;
 }
-
 // on est sorti de la boucle, c'est que le nombre saisi est bien le nombre cherché
 // on affiche un message de victoire
-alert('Bravo ! C\'était bien ' + searchedNumber + ' - Nombre d\'essais : ' + attemps);
 
+
+
+
+play(game)
