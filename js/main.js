a65a8de8e2b98beb11a70a7cb9a423d85e5dc71d
Ryad.253 <ryad.elm@icloud.com>
2025-05-09 16:05:05 +0200

    je suis fatigue

--- js/main.js
+++ js/main.js
@@ -1,28 +1,47 @@
-// Le nombre max
-var max = 500; 
 
-// Le nombre cherché
-var searchedNumber = Math.round(Math.random() * max);
+let game = {
+    enteredNumber: 0,
+    nombreEssais: 1,
+    minimum: 10,
+    maximum: 20 
+}
+let searchedNumber = Math.floor(Math.random() * (game.maximum - game.minimum + 1)) + game.minimum;
+let score = []
 
-// Le nombre saisi
-var enteredNumber = parseInt(prompt('Quel est le nombre à trouver ?'));
-
-// Le nombre d'essais
-var attemps = 1;
-
-// Tant que le nombre saisi n'est pas bon on redemande un nombre
-while (enteredNumber !== searchedNumber) {
-    // on précise si le nombre recherché est inférieur ou supérieur au nombre saisi
-    if (enteredNumber < searchedNumber) {
-        enteredNumber = parseInt(prompt('C\'est plus'));
-    }
-    else {
-        enteredNumber = parseInt(prompt('C\'est moins'));
-    }
-    // on incrémente le nombre d'essais
-    attemps += 1;
+function reset() {
+    searchedNumber = nombreAleatoire(game.minimum, game.maximum),
+    game.nombreEssais = 1;
+    game.enteredNumber = parseInt(prompt('Quel est le nombre à trouver ?'));
 }
 
-// on est sorti de la boucle, c'est que le nombre saisi est bien le nombre cherché
-// on affiche un message de victoire
-alert('Bravo ! C\'était bien ' + searchedNumber + ' - Nombre d\'essais : ' + attemps);
+function play(game) {
+    reset()
+    while (game.enteredNumber !== searchedNumber) {
+        // on précise si le nombre recherché est inférieur ou supérieur au nombre saisi
+        if (game.enteredNumber < searchedNumber) {
+            game.enteredNumber = parseInt(prompt('C\'est plus'));
+        }
+        else {
+            game.enteredNumber = parseInt(prompt('C\'est moins'));
+        }
+        // on incrémente le nombre d'essais
+        game.nombreEssais += 1;
+    }
+    alert('Bravo ! C\'était bien ' + searchedNumber + ' - Nombre d\'essais : ' + game.nombreEssais);
+    score.push(game.nombreEssais);
+    if (confirm("Voulez-vous rejouer ?")) {
+        play(game) 
+    }
+
+    for (let i = 0; i < score.length; i++) {
+        alert("Partie " + (i +1) + " : " + score[i] + " essais");
+    }
+    
+}
+
+function nombreAleatoire(min,max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+play(game)
+
