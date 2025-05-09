70429262e88bc18f4647da3f6ac0d9819e9d0f81
Gaël Pardonnet <gael.pardonnet@hotmail.fr>
2025-05-09 13:53:23 +0200

    j'ai oublié de faire les commits donc c'est fini sans trop tout décrire sinon ce serait un peu long

--- js/main.js
+++ js/main.js
@@ -1,28 +1,39 @@
-// Le nombre max
-var max = 500; 
+let game = {
+    min : 10, 
+    max :20,
+    attempts : 1,
+};
 
-// Le nombre cherché
-var searchedNumber = Math.round(Math.random() * max);
+let resultat = []
+let compteurPartie = 0
 
-// Le nombre saisi
-var enteredNumber = parseInt(prompt('Quel est le nombre à trouver ?'));
+function intervalleRandom(min, max) {
+    let searchedNumber = Math.round(Math.random() * (max - min) + min);
+    return searchedNumber;
+};
 
-// Le nombre d'essais
-var attemps = 1;
-
-// Tant que le nombre saisi n'est pas bon on redemande un nombre
-while (enteredNumber !== searchedNumber) {
-    // on précise si le nombre recherché est inférieur ou supérieur au nombre saisi
-    if (enteredNumber < searchedNumber) {
-        enteredNumber = parseInt(prompt('C\'est plus'));
+function play(game) {
+    let enteredNumber = parseInt(prompt('Saisi un nombre !'));
+    let searchedNumber = intervalleRandom(game.min, game.max);
+    while (enteredNumber !== searchedNumber) {
+        // on précise si le nombre recherché est inférieur ou supérieur au nombre saisi
+        if (enteredNumber < searchedNumber) {
+            enteredNumber = parseInt(prompt('C\'est plus'));
+        }
+        else {
+            enteredNumber = parseInt(prompt('C\'est moins'));
+        };
+        // on incrémente le nombre d'essais
+        game.attempts += 1;
+    };
+    compteurPartie++
+    resultat.push(' Partie ' + compteurPartie + ' : ' + game.attempts + " essais");
+    alert('Bravo ! C\'était bien ' + searchedNumber + ' - Nombre d\'essais : ' + game.attempts + ' Score de toutes les parties : ' + resultat);
+    if (confirm('Veux-tu rejouer ?')) {
+        return play(game)
+    } else {
+        return alert('Score final : ' + resultat)
     }
-    else {
-        enteredNumber = parseInt(prompt('C\'est moins'));
-    }
-    // on incrémente le nombre d'essais
-    attemps += 1;
-}
+};
 
-// on est sorti de la boucle, c'est que le nombre saisi est bien le nombre cherché
-// on affiche un message de victoire
-alert('Bravo ! C\'était bien ' + searchedNumber + ' - Nombre d\'essais : ' + attemps);
+play(game);
