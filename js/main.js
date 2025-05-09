93d3881ffb2528ea0573c08959b0996749533474
Taaazzz <taaazzz.bg@gmail.com>
2025-05-09 13:21:12 +0200

    Envoi du code pour le jeu de lagaffe

--- js/main.js
+++ js/main.js
@@ -1,28 +1,54 @@
-// Le nombre max
-var max = 500; 
+//Etape 1 cree un object avec les parametres du jeu
+const game = {
+    min : 1,
+    max: 500,
+    searchedNumber: null,
+    attempts: 0,
+    scores: []
+};
+//Etape 2 cree une fonction pour generez un nombre aleatoire
+function getRandomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+//Etape 3 cree une fonction pour jouer
+function play() {
+    //On initialise le jeu
+    game.searchedNumber = getRandomNumber(game.min, game.max);
+    game.attempts = 0;
 
-// Le nombre cherché
-var searchedNumber = Math.round(Math.random() * max);
+    //on va demander aux joueurs de nous donner un nombre
+    let entredNumber = parseInt(prompt(`Quel est le nombre à trouver entre ${game.min} et ${game.max} ?`));
 
-// Le nombre saisi
-var enteredNumber = parseInt(prompt('Quel est le nombre à trouver ?'));
-
-// Le nombre d'essais
-var attemps = 1;
-
-// Tant que le nombre saisi n'est pas bon on redemande un nombre
-while (enteredNumber !== searchedNumber) {
-    // on précise si le nombre recherché est inférieur ou supérieur au nombre saisi
-    if (enteredNumber < searchedNumber) {
-        enteredNumber = parseInt(prompt('C\'est plus'));
+    //Ici on va cree la boucle du jeu
+    //Si le nombre donné par le joueur n'est pas egal au nombre aleatoire
+    while (entredNumber !== game.searchedNumber) {
+        //On va lui dire que si plus petit ou plus grand ce qu'il doit faire
+        //Si le nombre donné par le joueur est plus petit que le nombre aleatoire
+        if (entredNumber < game.searchedNumber) {
+            entredNumber = parseInt(prompt('C\'est plus'));
+        } else {
+            entredNumber = parseInt(prompt('C\'est moins'));
+        }
+        //Pour savoir le nombre d'essai du joueur on va incrémenter sa valeur qui était a zéro
+        game.attempts += 1;
     }
-    else {
-        enteredNumber = parseInt(prompt('C\'est moins'));
-    }
-    // on incrémente le nombre d'essais
-    attemps += 1;
+}
+//Etape 4 on va stocker les scores
+game.scores.push(game.attempts);
+
+//Si le joueur a trouvé le bon nombre
+alert(`Bravo ! C'était bien ${game.searchedNumber} - Nombre d'essais : ${game.attempts}`);
+
+//Si le joueur gagne on peut lui proposer de rejouer si il le veut
+//On va utiliser la fonction prévue a cet effet confirm()
+if (confirm('Voulez-vous rejouer ?')) {
+    play();
 }
 
-// on est sorti de la boucle, c'est que le nombre saisi est bien le nombre cherché
-// on affiche un message de victoire
-alert('Bravo ! C\'était bien ' + searchedNumber + ' - Nombre d\'essais : ' + attemps);
+//maintenant on va enfin lancer notre fonction pour jouer
+play();
+
+
+
+//Remerciement a google pour m'avoir aider a trouver la solution XD
+
