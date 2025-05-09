778b067cbcc88fc56be28ec3064ef81daec02eb6
Quentyn404 <quentyn.petitjean@hotmail.com>
2025-05-09 14:20:31 +0200

    exo sans tab scores/bonus

--- js/main.js
+++ js/main.js
@@ -1,28 +1,38 @@
 // Le nombre max
-var max = 500; 
+let max = 10;
+const game = {
+  // Le nombre cherché
+  searchedNumber: Math.round(Math.random() * max),
 
-// Le nombre cherché
-var searchedNumber = Math.round(Math.random() * max);
-
-// Le nombre saisi
-var enteredNumber = parseInt(prompt('Quel est le nombre à trouver ?'));
-
-// Le nombre d'essais
-var attemps = 1;
-
-// Tant que le nombre saisi n'est pas bon on redemande un nombre
-while (enteredNumber !== searchedNumber) {
+  // Le nombre d'essais
+  attemps: 1,
+};
+function play(object) {
+  let enteredNumber = parseInt(prompt("Quel est le nombre à trouver ?"));
+  // Tant que le nombre saisi n'est pas bon on redemande un nombre
+  while (enteredNumber !== object.searchedNumber) {
     // on précise si le nombre recherché est inférieur ou supérieur au nombre saisi
-    if (enteredNumber < searchedNumber) {
-        enteredNumber = parseInt(prompt('C\'est plus'));
-    }
-    else {
-        enteredNumber = parseInt(prompt('C\'est moins'));
+    if (enteredNumber < object.searchedNumber) {
+      enteredNumber = parseInt(prompt("C'est plus"));
+    } else {
+      enteredNumber = parseInt(prompt("C'est moins"));
     }
     // on incrémente le nombre d'essais
-    attemps += 1;
-}
+    object.attemps += 1;
+  }
 
-// on est sorti de la boucle, c'est que le nombre saisi est bien le nombre cherché
-// on affiche un message de victoire
-alert('Bravo ! C\'était bien ' + searchedNumber + ' - Nombre d\'essais : ' + attemps);
+  // on est sorti de la boucle, c'est que le nombre saisi est bien le nombre cherché
+  // on affiche un message de victoire
+  alert(
+    "Bravo ! C'était bien " +
+      object.searchedNumber +
+      " - Nombre d'essais : " +
+      object.attemps
+  );
+}
+play(game);
+
+function aléatoire(min, max) {
+  return Math.floor(Math.random() * (max - min) + min);
+}
+alert(aléatoire(10, 20));
